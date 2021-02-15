21f3e33616b354b4e61ed77eea8855aaa5aa58cc
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-02-15 16:17:58 -0600

    added pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// prompt for questions
+//Description
+//Table of contents
+//installation
+//usage
+//license
+//contribution
+//tests
+//questions
+
+//build readme.md
+
+//choose license add badge near top notice added to license section
+
+//github username added to Questions with link to profiel
+
+//email address added to questions how to reach instructions
+
+//table of contents link to section of readme
+
+
+
